a4e5d9bb71c4e6ef9d448141a8715a98deeeeff2
xiaofeng_drew <xiaofeng_drew@outlook.com>
2025-09-22 21:14:28 +0800

    CI测试1

--- a/test_a1.py
+++ a/test_a1.py
@@ -1,10 +1,10 @@
 from time import sleep
 
-from playwright.sync_api import sync_playwright
-with sync_playwright() as p:
-    # 可以启动不同的浏览器：chromium, firefox, webkit
-    browser = p.chromium.launch(headless=False)  # headless=False 表示显示浏览器窗口
-    page = browser.new_page()
+import re
+from playwright.sync_api import Page, expect
+
+
+def test_example(page: Page) -> None:
     page.goto("https://demo.playwright.dev/todomvc/#/")
     page.get_by_role("textbox", name="What needs to be done?").type("abc",delay=100)
     page.get_by_role("textbox", name="What needs to be done?").press("Enter")
@@ -12,7 +12,5 @@ with sync_playwright() as p:
     Todo=page.get_by_role("link",name="TodoMVC",exact=True).get_attribute("href")
     print(Todo)
     sleep(5)
-    print(page.title())  # 打印页面标题
-    browser.close()
-
+    assert page.title =="React • TodoMVC"  # 打印页面标题
 
